fb26262092ca885383b25268727cf04d4544ecce
Jiahang Li <jiahang_li@outlook.com>
2014-11-08 15:50:31 -0800

    [Tak] Working on coinbase login

---
+++ public/js/coinbase.js
@@ -0,0 +1,7 @@
+var REDIRECT_URI = "";
+var CLIENT_ID = "";
+
+var URI = "https://www.coinbase.com/oauth/authorize?response_type=code&" +
+          "&client_id=" + CLIENT_ID + "&redirect_uri=" + REDIRECT_URI;
+
+$.ajax();
