cff320393d531a728ed650946c1199e91642c543
Jiahang Li <jiahang_li@outlook.com>
2014-11-08 18:40:59 -0800

    [Tak] debugging

--- public/js/coinbase.js
+++ public/js/coinbase.js
@@ -18,22 +18,24 @@ window.isUserLoggedIn = function () {
     var token_uri = ACCESS_TOKEN_URI + "?grant_type=authorization_code&code=" +
                     code + "&redirect_uri=" + REDIRECT_URI + "&client_id=" +
                     CLIENT_ID + "&client_secret=" + CLIENT_SECRET;
-    $.ajax(token_uri, {
-      type: "POST",
-      crossDomain : true,
-      xhrFields: {
-        withCredentials: true
-      },
-      success: function (data) {
-        alert(data);
-        alert(JSON.stringify(data));
-      },
-      error: function (err) {
-        alert(err);
-        alert(JSON.stringify(err));
-      }
-    });
+    console.log(code);
+    console.log(token_uri);
+    // $.ajax(token_uri, {
+    //   type: "POST",
+    //   crossDomain : true,
+    //   xhrFields: {
+    //     withCredentials: true
+    //   },
+    //   success: function (data) {
+    //     alert(data);
+    //     alert(JSON.stringify(data));
+    //   },
+    //   error: function (err) {
+    //     alert(err);
+    //     alert(JSON.stringify(err));
+    //   }
+    // });
   }
 }
 
-//https://www.coinbase.com/oauth/authorize?grant_type=authorization_code&code=05cded88ff915bb76ae0b109b78ce9d98cd0d8050c34c565188cddb4b7c05cda&redirect_uri=https://bitrunapp.herokuapp.com&client_id=68cfc8c475c6730bd046e4da8039a76edfa76f913bf58d7d0ddaffe937360139&client_secret=d376095308bb244127abfa069cb9aff9787f5542238051b9e3d67cbf0fa2477d
+https://www.coinbase.com/oauth/authorize?grant_type=authorization_code&code=05cded88ff915bb76ae0b109b78ce9d98cd0d8050c34c565188cddb4b7c05cda&redirect_uri=https://bitrunapp.herokuapp.com&client_id=68cfc8c475c6730bd046e4da8039a76edfa76f913bf58d7d0ddaffe937360139&client_secret=d376095308bb244127abfa069cb9aff9787f5542238051b9e3d67cbf0fa2477d
