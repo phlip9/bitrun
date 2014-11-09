a5cf0915489ac4b1296a7d93e5fcde66b6d4460d
Jiahang Li <jiahang_li@outlook.com>
2014-11-08 17:45:42 -0800

    [Tak] Change Server

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(function(req, res, next){
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
 
 // connect to database
 var path = process.env.MONGOHQ_URL + "/bitrun";
