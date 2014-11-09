a0fea7a3f6e2e303fb3a4a579ac4d1c2b34bc409
Jiahang Li <jiahang_li@outlook.com>
2014-11-09 01:18:46 -0800

    [Tak] Improving React

--- public/js/reactjs/react.js
+++ public/js/reactjs/react.js
@@ -5,7 +5,7 @@ window.renderIncentiveReact = function(incentive) {
 	var APP = React.createClass({displayName: 'APP',
 		getDefaultProps: function() {
 			return {
-					miles: incentive.goal,
+					km: incentive.goal,
 					create: incentive.create_date,
 					expire: incentive.expire_date,
 					amount: incentive.amount
@@ -14,29 +14,29 @@ window.renderIncentiveReact = function(incentive) {
 		getInitialState: function () {
 			return {
 				percent: "Calculating..."
-			}
+			};
 		},
 		render: function () {
 			return (
-				React.createElement("div", null,
-					React.createElement("div", {className: "row page-header thin"},
-						"Your Goal: Run ", this.props.miles, " Miles in ", this.props.days, " days"
-					),
-					React.createElement("div", {className: "row"},
-						React.createElement("div", {className: "col-md-6"},
-							React.createElement("div", {className: "row"}, "Created On: ", this.props.create),
-							React.createElement("div", {className: "row"}, "Expires On: ", this.props.expire),
-							React.createElement("div", {className: "row"}, "Bitcoins Bid: ", this.props.amount),
+				React.createElement("div", null, 
+					React.createElement("div", {className: "row page-header thin"}, 
+						"Your Goal: Run ", this.props.km, " km in ", this.props.days, " days"
+					), 
+					React.createElement("div", {className: "row"}, 
+						React.createElement("div", {className: "col-md-6"}, 
+							React.createElement("div", {className: "row"}, "Created On: ", this.props.create), 
+							React.createElement("div", {className: "row"}, "Expires On: ", this.props.expire), 
+							React.createElement("div", {className: "row"}, "Bitcoins Bid: ", this.props.amount), 
 							React.createElement("div", {className: "row"}, "Percent Done: ", this.state.percent)
-						),
-						React.createElement("div", {className: "col-md-6"},
-							React.createElement("div", null, "Analytics"),
-							React.createElement("div", null, "This should be a D3 fancy graph"),
+						), 
+						React.createElement("div", {className: "col-md-6"}, 
+							React.createElement("div", null, "Analytics"), 
+							React.createElement("div", null, "This should be a D3 fancy graph"), 
 							React.createElement("button", {onClick: "createSocketConnection()"})
 						)
 					)
 				)
-			)
+			);
 		}
 	});
 
@@ -44,4 +44,51 @@ window.renderIncentiveReact = function(incentive) {
 
 };
 
-window.setIncentiveReact = function () {};
+window.setIncentiveReact = function (id) {
+
+	var APP2 = React.createClass({displayName: 'APP2',
+		mixins: [React.addons.LinkedStateMixin],
+		submit: function() {
+			console.log(JSON.stringify({
+				km: this.state.km,
+				length: this.state.length,
+				amount: this.state.amount,
+				id: id
+			}));
+		},
+		getInitialState: function () {
+			return {
+				km: 0,
+				length: 7,
+				amount: 0
+			};
+		},
+		render: function () {
+			return (
+        React.createElement("form", null, 
+            React.createElement("div", null, 
+								React.createElement("span", null, "I want to run:"), 
+                React.createElement("input", {valueLink: this.linkState('km'), type: "number"}), 
+                React.createElement("label", null, this.state.km), 
+								React.createElement("span", null, "km.")
+            ), 
+						React.createElement("div", null, 
+								React.createElement("span", null, "I want to keep running for:"), 
+								React.createElement("input", {valueLink: this.linkState('length'), type: "number"}), 
+								React.createElement("label", null, this.state.length), 
+								React.createElement("span", null, "days.")
+						), 
+						React.createElement("div", null, 
+								React.createElement("span", null, "I'm willing to bid:"), 
+								React.createElement("input", {valueLink: this.linkState('amount'), type: "number"}), 
+								React.createElement("label", null, this.state.amount), 
+								React.createElement("span", null, "BitCoins.")
+						), 
+						React.createElement("button", {className: "btn btn-success", onClick: this.submit}, "Go")
+        )
+			);
+		}
+	});
+
+	React.renderComponent(React.createElement(APP2, null), document.getElementById("create"));
+};
--- public/js/reactjsx/react.js
+++ public/js/reactjsx/react.js
@@ -5,7 +5,7 @@ window.renderIncentiveReact = function(incentive) {
 	var APP = React.createClass({
 		getDefaultProps: function() {
 			return {
-					miles: incentive.goal,
+					km: incentive.goal,
 					create: incentive.create_date,
 					expire: incentive.expire_date,
 					amount: incentive.amount
@@ -14,13 +14,13 @@ window.renderIncentiveReact = function(incentive) {
 		getInitialState: function () {
 			return {
 				percent: "Calculating..."
-			}
+			};
 		},
 		render: function () {
 			return (
 				<div>
 					<div className="row page-header thin">
-						Your Goal: Run {this.props.miles} Miles in {this.props.days} days
+						Your Goal: Run {this.props.km} km in {this.props.days} days
 					</div>
 					<div className="row">
 						<div className="col-md-6">
@@ -36,7 +36,7 @@ window.renderIncentiveReact = function(incentive) {
 						</div>
 					</div>
 				</div>
-			)
+			);
 		}
 	});
 
@@ -44,4 +44,51 @@ window.renderIncentiveReact = function(incentive) {
 
 };
 
-window.setIncentiveReact = function () {};
+window.setIncentiveReact = function (id) {
+
+	var APP2 = React.createClass({
+		mixins: [React.addons.LinkedStateMixin],
+		submit: function() {
+			console.log(JSON.stringify({
+				km: this.state.km,
+				length: this.state.length,
+				amount: this.state.amount,
+				id: id
+			}));
+		},
+		getInitialState: function () {
+			return {
+				km: 0,
+				length: 7,
+				amount: 0
+			};
+		},
+		render: function () {
+			return (
+        <form>
+            <div>
+								<span>I want to run:</span>
+                <input valueLink={this.linkState('km')} type="number" />
+                <label>{this.state.km}</label>
+								<span>km.</span>
+            </div>
+						<div>
+								<span>I want to keep running for:</span>
+								<input valueLink={this.linkState('length')} type="number" />
+								<label>{this.state.length}</label>
+								<span>days.</span>
+						</div>
+						<div>
+								<span>I'm willing to bid:</span>
+								<input valueLink={this.linkState('amount')} type="number" />
+								<label>{this.state.amount}</label>
+								<span>BitCoins.</span>
+						</div>
+						<button className="btn btn-success" onClick={this.submit}>Go</button>
+        </form>
+			);
+		}
+	});
+
+	React.renderComponent(<APP2 />, document.getElementById("create"));
+};
